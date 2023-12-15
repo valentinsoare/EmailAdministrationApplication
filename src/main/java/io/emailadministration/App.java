7052b8e0f646fd68c224f470be4dbce2cbf92fd0
Valentin Soare <soarevalentinn@gmail.com>
2023-12-15 13:31:30 +0200

    Defined ConnectionToDB packages along with appropiate classes made following Singleton Design Pattern!

--- src/main/java/io/emailadministration/App.java
+++ src/main/java/io/emailadministration/App.java
@@ -1,13 +1,7 @@
 package io.emailadministration;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+
     }
 }
